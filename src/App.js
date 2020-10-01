960ab24065d567cd4516097b9a5ddc50d516163c
cawilson1 <cawilson1@g.cofc.edu>
2020-10-01 10:32:40 -0400

    redirect on sign in and have a signed in state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PublicRoutes from "./components/PublicRoutes";
 import Navbar from "./components/Navbar";
 function App() {
+  const [signedIn, setSignedIn] = useState(undefined);
+  useEffect(() => {
+    if (window.localStorage.jwt) {
+      setSignedIn(window.localStorage.jwt);
+    }
+  }, []);
+  console.log(signedIn);
   return (
     <div>
       <Navbar />
